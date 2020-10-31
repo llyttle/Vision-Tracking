5e18257607294b265b00e72d67204052d287f9a5
llyttle <llyttle@olin.edu>
2020-10-31 19:26:31 -0400

    Derive ball position in degrees from ball_binary_image

--- neato_soccer_player/scripts/neato_soccer_node.py
+++ neato_soccer_player/scripts/neato_soccer_node.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Image, LaserScan
 from copy import deepcopy
 from cv_bridge import CvBridge
 import cv2
+import math
 import numpy as np
 from geometry_msgs.msg import Twist, Vector3
 
@@ -22,20 +23,24 @@ class BallTracker(object):
         """ Initialize the ball tracker """
         rospy.init_node('ball_tracker')
         #the latest image from the camera
-        self.cv_image = None                        
+        self.cv_image = None                  
+        self.scan_topic = "scan"      
         #used to convert ROS messages to OpenCV
         self.bridge = CvBridge()
 
         #ceate publishers and subscribers
         #create a subscriber to the camera topic
+        rospy.Subscriber(self.scan_topic, LaserScan, self.pixel_to_degrees)
+
         rospy.Subscriber(image_topic, Image, self.process_image)
         #create a publisher to drive the robot
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        self.lidar_array = []
         
         #create an open cv visualization window
         cv2.namedWindow('video_window')
         #create a call back function for when the image in the window is clicked on
-        cv2.setMouseCallback('video_window', self.process_mouse_event)
+    #    cv2.setMouseCallback('video_window', self.process_mouse_event)
 
     def process_mouse_event(self, event, x,y,flags,param):
         """ A function that is called when the mouse clicks on the open camera window. Function displays a popup with text describing the color value of the camera pixel you clicked on"""
@@ -47,24 +52,29 @@ class BallTracker(object):
         cv2.imshow('image_info', image_info_window)
         cv2.waitKey(5)
 
-
     def process_image(self, msg):
         """ Process image messages from ROS and stash them in an attribute
             called cv_image. """
         self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
+        moments = cv2.moments(self.ball_binary_image)
+        if moments['m00'] != 0:
+            self.center_x, self.center_y = moments['m10']/moments['m00'], moments['m01']/moments['m00']
+            #print(self.center_x, self.center_y)
+
     def filter_image(self):
         """A function which filters self.cv_image into binary images which can be used for processing in decision and navigational algorithms."""
         self.ball_binary_image = cv2.inRange(self.cv_image, (0,0,1), (10,10,255))
+
+    def pixel_to_degrees(self, msg):
+        vanish_angle = math.radians(43)                 #widest angle in image frame
+        f = -300/math.tan(vanish_angle)                 #variable representing camera focal distance
+
+        theta_rad = math.atan((self.center_x-300)/f)    #theta = atan(x/f)
+        theta = math.degrees(theta_rad)
+
+        print(theta)
         
-
-
-        
-    def get_laser_scan(self, msg):
-        for i in msg.ranges:
-            if i > 0:
-                print(i)
-
 
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
@@ -74,25 +84,27 @@ class BallTracker(object):
             #update the filtered binary images
             self.filter_image()
 
+            self.pixel_to_degrees
+
             #if there is a cv.image
-            if not self.cv_image is None:
+            #if not self.cv_image is None:
                 
                 #debug text
-                print("\n self.cv_image:")
-                print(self.cv_image.shape)
+            #    print("\n self.cv_image:")
+            #    print(self.cv_image.shape)
                 
                 #display self.cv_image
-                cv2.imshow('video_window', self.cv_image)
-                cv2.waitKey(5)
+            #    cv2.imshow('video_window', self.cv_image)
+            #    cv2.waitKey(5)
 
             if not self.ball_binary_image is None:
                 #debug text
-                print("\n self.ball_binary_image: "
-                print(self.ball_binary_image.shape)
+            #    print("\n self.ball_binary_image: ")
+            #    print(self.ball_binary_image.shape)
                 
                 #display the ball filter image
                 cv2.imshow('ball filter',self.ball_binary_image)
-                cv2.waitKey(5)           
+                cv2.waitKey(5)        
 
             # start out not issuing any motor commands
             r.sleep()
